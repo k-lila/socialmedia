d041a63c5a2cef55131c2e9dba5e73e4b4ba3109
k-lila <krishnalilapalandi@gmail.com>
2024-07-18 00:36:36 -0300

    profile pictures added to posts, profile and etc

--- xuitter/models/profile.py
+++ xuitter/models/profile.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
         "self", related_name="followed_by", symmetrical=False, blank=True
     )
     date_modified = models.DateTimeField(User, auto_now=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
 
     def __str__(self):
         return self.user.username
--- xuitter/views/update_user.py
+++ xuitter/views/update_user.py
@@ -2,18 +2,30 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-from ..forms import SignUpForm
+from ..models import Profile
+from ..forms import SignUpForm, ProfileImgForm
 
 
 def update_user(request):
     if request.user.is_authenticated:
         current_user = User.objects.get(id=request.user.id)
-        form = SignUpForm(request.POST or None, instance=current_user)
-        if form.is_valid():
-            form.save()
+        profile_user = Profile.objects.get(user__id=request.user.id)
+        user_form = SignUpForm(
+            request.POST or None, request.FILES or None, instance=current_user
+        )
+        profile_form = ProfileImgForm(
+            request.POST or None, request.FILES or None, instance=profile_user
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
             login(request, current_user)
             messages.success(request, ("modificações salvas"))
             return redirect("home")
-        return render(request, "update_user.html", {"form": form})
+        return render(
+            request,
+            "update_user.html",
+            {"user_form": user_form, "profile_form": profile_form},
+        )
     else:
         return redirect("home")
