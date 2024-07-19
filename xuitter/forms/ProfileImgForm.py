c1551c671c1b9c3deba3ee07acdde21491cb05e3
k-lila <krishnalilapalandi@gmail.com>
2024-07-19 00:43:50 -0300

    profile bio & personal website

--- xuitter/forms/ProfileImgForm.py
+++ xuitter/forms/ProfileImgForm.py
@@ -6,7 +6,21 @@ from ..models import Profile
 
 class ProfileImgForm(forms.ModelForm):
     profile_image = forms.ImageField(label="Profile image")
+    profile_bio = forms.CharField(
+        label="bio",
+        widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "bio"}),
+    )
+    profile_website = forms.CharField(
+        label="website",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "website"}
+        ),
+    )
 
     class Meta:
         model = Profile
-        fields = ("profile_image",)
+        fields = (
+            "profile_image",
+            "profile_bio",
+            "profile_website",
+        )
