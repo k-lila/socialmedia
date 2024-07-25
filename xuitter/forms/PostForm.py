60a06193648eb24a87b25e099e98186494e540bc
k-lila <krishnalilapalandi@gmail.com>
2024-07-24 23:45:47 -0300

    styles, css, added

--- xuitter/forms/PostForm.py
+++ xuitter/forms/PostForm.py
@@ -5,10 +5,11 @@ from ..models import Post
 class PostForm(forms.ModelForm):
     body = forms.CharField(
         required=True,
+        max_length=140,
         widget=forms.widgets.Textarea(
             attrs={
                 "placeholder": "Escreva seu post",
-                "class": "form-control",
+                "class": "form-control post-form__form",
             }
         ),
         label="",
