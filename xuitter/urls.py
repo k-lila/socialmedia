28c283737f28f1c6f38121e9b0b3342b034e5757
k-lila <krishnalilapalandi@gmail.com>
2024-07-26 02:32:11 -0300

    wip

--- xuitter/urls.py
+++ xuitter/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     path("delete_post/<int:pk>", delete_post, name="delete_post"),
     path("search/", search, name="search"),
     path("search_user/", search_user, name="search_user"),
+    path("update_server/", update, name="update"),
+    path("hello/", hello_world, name="hello_world"),
 ]
